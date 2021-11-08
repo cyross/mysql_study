0fb8ed5e61b8639cf0c28d59b49898ffa7ab4421
Cyross Makoto <takuan45189@gmail.com>
2021-11-09 00:18:06 +0900

    テーブルを追加

--- docker/mysql/work/join1.sql
+++ docker/mysql/work/join1.sql
@@ -9,3 +9,17 @@ insert into `join1` VALUES
     ('林檎', 'リンゴ'),
     ('蜜柑', 'ミカン'),
     ('梨', 'ナシ');
+
+drop table if exists `join2`;
+
+create table `join2` (
+    `name` varchar(20) not null,
+    `yomi` varchar(20) not null,
+    `price` int not null
+);
+
+insert into `join2` VALUES
+    ('林檎', 'リンゴ', 100),
+    ('蜜柑', 'ミカン', 80),
+    ('梨', 'ナシ', 100),
+    ('葡萄', 'ブドウ', 200);
--- docker/mysql/work/join1_query.sql
+++ docker/mysql/work/join1_query.sql
@@ -8,6 +8,11 @@ select * from `join1`as p1 inner join `join1` as p2 on p1.name > p2.name;
 create table `ptmp_3` as select row_number() over () as `px`, p1.`name` as `px1`, p2.`name` as `px2` from `join1`as p1 inner join `join1` as p2 on p1.name > p2.name;
 select `px1`, `px2` from `ptmp_3` where `px` = (select max(`px`) from `ptmp_3`);
 drop table if exists `ptmp_3`;
+\! echo 'ランク1'
+select rank() over (order by `price`) as `px`, `name` from `join2`;
+\! echo 'ランク2'
+select dense_rank() over (order by `price`) as `px`, `name` from `join2`;
+
 
 -- これはエラーになる
 -- select * from (select row_number() over () as `px`, p1.`name` as `px1`, p2.`name` as `px2` from `join1`as p1 inner join `join1` as p2 on p1.name > p2.name) order by `px` desc limit 1;
